f026c5f03fdb18ae2a03b829a7e0dedb245795cd
Bhavana-Uppala <uppalabhavana3@gmail.com>
2025-05-19 17:09:13 +0530

    My first spring boot

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -1,5 +1,13 @@
 package com.example.demo;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class HomeController {
+    @RequestMapping("/")
+    public class index {
+        return "index.html";
+    }
 
 }
